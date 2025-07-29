b40313a2cfd880c693af3c99edb4b2c862cddd96
SantiCarvajalJulius <santiago.carvajal@julius2grow.com>
2025-07-29 12:02:19 -0500

    Quitando lo de eliminar los mensajes cada que se reinicie

--- services/db.py
+++ services/db.py
@@ -15,9 +15,6 @@ def get_connection():
 def init_db():
     conn = get_connection()
     c    = conn.cursor()
-
-    c.execute("DROP TABLE IF EXISTS mensajes;")
-
     # mensajes
     c.execute("""
     CREATE TABLE IF NOT EXISTS mensajes (
