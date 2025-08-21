30a36427727305e756fe6329b3b407cc3ba8c8b0
SantiagoCTB <90730582+SantiagoCTB@users.noreply.github.com>
2025-08-20 20:09:50 -0500

    refactor: replace ChatTecnimedellin with ChatInterface

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ChatTecnimedellin from './ChatTecnimedellin';
+import ChatInterface from './components/ChatInterface';
 import './index.css';
 
 interface SessionData {
@@ -14,6 +14,6 @@ const sessionData: SessionData = dataEl ? JSON.parse(dataEl.textContent || '{}')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChatTecnimedellin role={sessionData.role} roleId={sessionData.roleId} sessionRoles={sessionData.sessionRoles} />
+    <ChatInterface role={sessionData.role} roleId={sessionData.roleId} sessionRoles={sessionData.sessionRoles} />
   </React.StrictMode>
 );
