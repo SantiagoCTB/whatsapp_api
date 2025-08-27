c98f58f9290576856f740c477970cc7b08d96be1
SantiagoCTB <90730582+SantiagoCTB@users.noreply.github.com>
2025-08-27 08:47:06 -0500

    Use configurable Streamlit iframe URL

--- routes/streamlit_routes.py
+++ routes/streamlit_routes.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, render_template_string, request
+import os
+
+from flask import Blueprint, render_template_string
+
+from config import Config
 
 streamlit_bp = Blueprint('streamlit', __name__, url_prefix='/streamlit')
 
 @streamlit_bp.route('/')
 def dashboard():
     """Render an iframe embedding the Streamlit dashboard."""
+    iframe_url = os.getenv("STREAMLIT_URL", Config.STREAMLIT_URL)
     iframe_html = (
-        f"<iframe src='http://{request.host.split(':')[0]}:8501' style='width:100%; height:100vh; border:none;'></iframe>"
+        f"<iframe src='{iframe_url}' style='width:100%; height:100vh; border:none;'></iframe>"
     )
     return render_template_string(iframe_html)
