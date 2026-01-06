db11b55a4323cc72ae1573e89c0192694a0976ab
SantiagoCTB <90730582+SantiagoCTB@users.noreply.github.com>
2026-01-06 11:25:46 -0500

    Serve legal landing pages

---
+++ routes/landing_routes.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Blueprint, send_from_directory
+
+from config import Config
+
+landing_bp = Blueprint("landing", __name__)
+
+LANDING_DIR = os.path.join(Config.BASEDIR, "landing")
+
+
+@landing_bp.route("/privacidad")
+def privacidad():
+    return send_from_directory(LANDING_DIR, "privacidad.html")
+
+
+@landing_bp.route("/aviso")
+def aviso():
+    return send_from_directory(LANDING_DIR, "aviso.html")
+
+
+@landing_bp.route("/terminos")
+def terminos():
+    return send_from_directory(LANDING_DIR, "terminos.html")
